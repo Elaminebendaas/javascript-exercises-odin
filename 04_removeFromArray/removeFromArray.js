8599a4295e7a556748d5e4555ced46ff8ff45a2d
ElamineBendaas <33202205+Elaminebendaas@users.noreply.github.com>
2022-11-27 19:19:55 -0500

    didnt finish

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(content, removing) {
+    let length = content.length
+    console.log(removing)
+    for(i = 0; i<length;i++){
+        if(content[i] == removing){
+            content.splice(i, 1)
+            console.log(i)
+        }
+    }
+    return content
 };
 
 // Do not edit below this line
