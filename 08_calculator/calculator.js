0af30658bd180f5f525fa177051e8520d3dc0442
ElamineBendaas <33202205+Elaminebendaas@users.noreply.github.com>
2022-11-27 19:15:25 -0500

    finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a , b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0
+	array.forEach(element => {
+    sum += element
+  });
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.length
+      ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+      : 0;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if(n === 0 ) return 1
+  let product = 1
+  for (let i = n; i > 0; i--){
+    product *= i
+  }
+  return product
 };
 
 // Do not edit below this line
@@ -31,3 +42,5 @@ module.exports = {
   power,
   factorial
 };
+
+
